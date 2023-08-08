c9a326019c3a391f0bb6f4429e6ef2b1c9581d8a
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-08-09 00:02:29 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/* console.log('Hola, mundo 🖐');
+console.info('Hola, mundo 🖐');
+console.error('Hola, mundo 🖐'); */
+// console.log(window)  window no funciona en node al no ser una variable global (sólo lo es en el navegador)
+
+console.log(globalThis); //globalThis es una variable global de nodeJS
